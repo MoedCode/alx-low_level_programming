ba8c16d7d29e8293ac720d1bf1b79039e9e6f3da
MoedCode <mohamed71291@gmail.com>
2023-06-15 19:13:51 -0700

    0x00c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descrption : A C  Program Printing with Put Function 
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
