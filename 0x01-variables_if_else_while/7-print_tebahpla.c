77aef040222f10a36d75f313b413635217c7fdcc
MoedCode <mohamed71291@gmail.com>
2023-06-17 05:04:29 -0700

    kokooo

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Descreption :Print from a TO z "Alphabet"
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+*/
+int main(void)
+{
+	for (int i = 97; i < 123 ; i++)
+		putchar(i)
+}
