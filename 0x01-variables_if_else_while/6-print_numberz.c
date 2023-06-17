476c08a22b50cd3ff2759628a5173b85dd84c63f
MoedCode <mohamed71291@gmail.com>
2023-06-16 20:59:52 -0700

    T6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Descreption :Print 0-9 using putchat(par1 + '0')
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+*/
+int main(void)
+{
+	int chint = 0;
+
+	for (; chint < 9; chint++)
+	{
+		putchar(chint + '0');
+	}
+	putchar('\n');
+	return (0);
+}
