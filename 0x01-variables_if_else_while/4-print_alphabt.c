06b9d4f81b61285b33ce90196a3cd33b722ceb7a
MoedCode <mohamed71291@gmail.com>
2023-06-16 20:20:52 -0700

    oooo

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,25 +2,25 @@
 /**
  * main - entry point
  *
- * Descreption :Print from A TO Z "Alphabet"
+ * Descreption :Print from A TO Z "Alphabet" without 'q,e'
  *
  * Return: Always 0 (Success)
  *
  *
 */
 
-int main()
+int main(void)
 {
 	char ca = 'a';
 
-	while(ca <= 'a')
+	while (ca <= 'z')
 	{
-		if(ca == 'e' || ca === 'q')
+		if (ca == 'e' || ca == 'q')
 			ca++;
 		putchar(ca);
 		ca++;
 	}
 	putchar('\n');
 
-	return (0)
+	return (0);
 }
