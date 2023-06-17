854d72ce516c3c08505956d3cd0f079513fc40ce
MoedCode <mohamed71291@gmail.com>
2023-06-16 20:36:51 -0700

    T5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Descreption :Print from A TO Z "Alphabet"
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
