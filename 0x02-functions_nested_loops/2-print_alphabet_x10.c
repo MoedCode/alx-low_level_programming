0f148bcf8d94f7b3752430113d7c8cd4a88bb03e
MoedCode <mohamed71291@gmail.com>
2023-06-20 18:14:27 -0700

    al3na 3lykom (automated command)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - function that print the alphabet ten times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int k;
+
+		for (k = 'a'; k <= ; k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+}
