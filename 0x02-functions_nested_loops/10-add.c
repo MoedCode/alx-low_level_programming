c19336918520898aa045fd4ec1b8903a7356ce0e
MoedCode <mohamed71291@gmail.com>
2023-06-20 19:28:31 -0700

    al3na 3lykom (automated command)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * add - returns the sum of numbers
+ * @a:type int
+ * @s: type int
+ *
+ * Return: the the result
+ *
+ */
+int add(int a, int s)
+{
+	int res = x + y;
+
+	return (res);
+}
