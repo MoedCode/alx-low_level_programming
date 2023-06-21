6d93687b08f5b32c70eac2adc9a940b10290a822
MoedCode <mohamed71291@gmail.com>
2023-06-20 17:49:43 -0700

    al3na 3lykom (automated command)

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_aTOz - function  prints all  alphabet in lower case
+ *
+ */
+
+void printAlph(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
