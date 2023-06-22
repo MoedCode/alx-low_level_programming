022f76f521458f91a560b7b2796e6cec074dd7fb
MoedCode <mohamed71291@gmail.com>
2023-06-22 16:06:18 -0700

    Mozokashi desuni (automated command)

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
-
-int _isupper(int c);
 int _isdigit(int c);
-
+int _isupper(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_square(int size);
+void print_triangle(int size);
 #endif /* MAIN_H */
