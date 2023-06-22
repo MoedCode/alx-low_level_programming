bb28466ee89386af61ea81baa830e67b0d40592a
MoedCode <mohamed71291@gmail.com>
2023-06-22 16:16:38 -0700

    Mozokashi desuni (automated command)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+* _isupper -  multiplies two integers
+ *
+ * @a: from type int
+ *
+ * @b: from type int
+ *
+ * Return: the value of the last digit
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
