d4c31e8eafbc1eef5c631c6c375a7e9389e476ae
MoedCode <mohamed71291@gmail.com>
2023-06-22 17:19:01 -0700

    Mozokashi desuni (automated command)

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9 Exept 2,4
+ *
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	 _putchar('\n');
+}
