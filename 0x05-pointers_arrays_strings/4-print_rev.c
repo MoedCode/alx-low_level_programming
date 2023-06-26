202505971597139d084b67e727d1bb95569fd063
MoedCode <mohamed71291@gmail.com>
2023-06-26 16:53:59 -0700

    al 3ed far7a (automated command)

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include"main.h"
+/**
+ * _strlen -   returns the length of a string.
+ *
+ * @s: type char*
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int ln = 0;
+
+	while (s[ln] != '\0')
+		ln++;
+	return (ln);
+}
+
+/**
+ * print_rev -  prints a string, in reverse, new line
+ *
+ * @s: type char
+ *
+ * Return: void
+ *
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = _strlen(s) - 1; i >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
