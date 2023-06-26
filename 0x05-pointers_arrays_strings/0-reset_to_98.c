db90efffd328998f8f0a5650c2cb74c7c7e1c4d9
MoedCode <mohamed71291@gmail.com>
2023-06-26 15:53:57 -0700

    al 3ed far7a (automated command)

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * reset_to_98 - takes a pointer for int as parameter
+ *   updates ots  value it points to to 98.
+ *
+ *  @n: a pointer to a type int
+ *
+ * return: 0 if sucess
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
