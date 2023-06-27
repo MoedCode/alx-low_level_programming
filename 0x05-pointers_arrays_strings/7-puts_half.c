c304fa676023d033c6549ab0124e24fff5faeac4
MoedCode <mohamed71291@gmail.com>
2023-06-26 18:32:49 -0700

    al 3ed far7a (automated command)

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include"main.h"
+/**
+ * _strlen -  returns the length of a string.
+ *
+ * @s: type CHAR*
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int ln = 0;
+
+	while (s[ln] != '\0')
+		ln++;
+	return (ln);
+}
+/**
+ * puts_half -  print half str
+ * @str: char*
+ *
+ * return: Nothin baby
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	double nn = _strlen(str), mm = nn % 2;
+
+	if (mm != 0)
+		i = nn / 2 + 1;
+	else
+		i = nn / 2;
+	for (; i < nn; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
