12f5410d8fe5ce136c0e0942382e3cabc6e736e6
MoedCode <mohamed71291@gmail.com>
2023-06-26 17:38:21 -0700

    al 3ed far7a (automated command)

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	char s[] = "1234";
+	rev_string(s);
+	printf("%s dd", s);
+	return 0;
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,44 @@
+#include"main.h"
+/**
+ * _strlen -  Returns a length of a string.
+ *
+ * @s: type char*
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int ln = 0;
+
+	while (s[ln] != '\0')
+		ln++;
+	return (ln);
+}
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ *
+ * @str: type string
+ *
+ *
+ * return: void
+ */
+
+void puts2(char *str)
+{
+	int mode = 1, int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (mode == 1)
+		{
+		_putchar(str[i]);
+		mode = 0;
+		}
+
+		else
+			mode = 1;
+	}
+	_putchar('\n');
+}
