d2e146c4daf57e2c9b0975e78df429f12077de90
MoedCode <mohamed71291@gmail.com>
2023-06-27 14:06:02 -0700

    (automated command)

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+/**
+ * main - Enry Point generate random password
+ *
+ * Return: (0) On   Success any integer  else for  failer
+ */
+
+int main(void)
+{
+int i = 0, randN = 0, sumT = 0;
+char password[100],
+p[63] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+posChars[63] = p;
+
+	srand(time(NULL));
+	while (sumT < (2650))/*2772 -122*/
+	{
+		randN = rand() % 62;
+		password[i] = posChars[randN];
+		sumT += posChars[randN];
+		i++;
+	}
+	randN  = 2772 - sumT;
+	password[i] = randN;
+	password[++i] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
