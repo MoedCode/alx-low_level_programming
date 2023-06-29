3e6ea834c6f7b233b70610a7ff2839ba9d09dc5f
MoedCode <mohamed71291@gmail.com>
2023-06-29 16:27:54 -0700

    (automated command)

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - small letters to capital letters
+ * @a:argument type char* for sting will be change
+ *
+ * Return: a (string after the convertion)
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+
+		}
+	}
+	return (str);
+
+}
