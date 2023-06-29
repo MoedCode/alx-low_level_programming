31da4e45df126280d7f779ffcc3e615257fd2095
MoedCode <mohamed71291@gmail.com>
2023-06-29 14:55:34 -0700

    (automated command)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,36 @@
-#include "main.h"
-
+#include"main.h"
 /**
- * _strcmp - compares two string
+ * _strlen - returns the length of a string.
+ *
+ * @s: type char*
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int ln = 0;
+
+	while (s[ln] != '\0')
+		ln++;
+	return (ln);
+}
+/**
+ * _strcmp - compare strings
  * @s1: type char*
  * @s2: type char*
  *
- * Return: 0
+ * Return: (0) if both are equal , postive if the dont
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2 && *s1 != '\0')
+	while (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
 	{
-		s1++; 
+		s1++;
 		s2++;
 	}
-	return (unsigned s1 - unsigned s2);
+	if (*s1 != *s2)
 
+		return (*s1 - *s2);
+	else
+		return (0);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,9 @@
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
 #define MAX_SIZE 100
 
 #endif
