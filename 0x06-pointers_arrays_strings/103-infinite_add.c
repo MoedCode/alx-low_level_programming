fbd7de002e811f9e54d568d9b342a7c5a350efe0
MoedCode <mohamed71291@gmail.com>
2023-06-30 19:53:23 -0700

    (automated command)

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,53 @@
-#include "main.h"
-#include <stdio.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
+ * infinite_add - adds tp argument from char* as numbers
+ * @n1: type char*
+ * @n2: type char*
+ * @r: buffer size type int char*
+ * @size_r: buffer size: type int
  * Return: the pointer to dest.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, ij, ik, il, im, in;
+	/*declare counter varables*/
+	int i, i1, i2, i3, i4, i5;
 
 	for (i = 0; n1[i]; i++)
-		;
-	for (ij = 0; n2[ij]; ij++)
-		;
-	if (i > size_r || ij > size_r)
-		return (0);
-	im = 0;
-	for (i -= 1, ij -= 1, ik = 0; ik < size_r - 1; i--, ij--, ik++)
+	;
+
+	for (i1 = 0; n2[i1]; i1++)
+	;
+
+	/*checks if either i or j is greater than size_r.*/
+	if (i > size_r || i1 > size_r)
 	{
-		in = im;
+		return (0);
+	}
+	i4 = 0;
+	/*loop will stop when it reaches the limit of the buffer size*/
+	for (i -= 1, i1 -= 1, i2 = 0; i2 < size_r - 1; i--, i1--, i2++)
+	{
+		i5 = i4;
 		if (i >= 0)
-			in += n1[i] - '0';
-		if (ij >= 0)
-			in += n2[j] - '0';
-		if (i < 0 && j < 0 && in == 0)
+			i5 += n1[i] - '0';
+			if (i1 >= 0)
+			i5 += n2[i1] - '0';
+		/* If this is true, it breaks out of the loop.*/
+		if (i < 0 && i1 < 0 && i5 == 0)
 		{
 			break;
 		}
-		m = in / 10;
-		r[ik] = in % 10 + '0';
+		i4 = i5 / 10;
+		r[i2] = i5 % 10 + '0';
 	}
-	r[ik] = '\0';
-	if (i >= 0 || ij >= 0 || im)
+	r[i2] = '\0';
+	if (i >= 0 || i1 >= 0 || i4)
 		return (0);
-	for (ik -= 1, il = 0; l < ik; ik--, il++)
+	for (i2 -= 1, i3 = 0; i3 < i2; i2--, i3++)
 	{
-		im = r[ik];
-		r[ik] = r[l];
-		r[l] = im;
+		i4 = r[i2];
+		r[i2] = r[i3];
+		r[i3] = i4;
 	}
 	return (r);
 
