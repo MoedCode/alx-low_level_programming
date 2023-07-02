647a636c57b73e2c4460b22b34572afacc856a3d
MoedCode <mohamed71291@gmail.com>
2023-07-02 16:02:58 -0700

    (automated command)

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,58 +1,88 @@
-
 /**
- * infinite_add - adds tp argument from char* as numbers
- * @n1: type char*
- * @n2: type char*
- * @r: buffer size type int char*
- * @size_r: buffer size: type int
- * Return: the pointer to dest.
+ * len - calculates string length
+ * @str:type string
+ *
+ * Return: length
+ */
+int len(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * stringRevers - reverse a string by  replacing indexs values
+ * @x: - type char* to rverce
+ * @len: - char* length
+ * Return: reversed char*
+ */
+
+char *stringRevers(char *x)
+{
+	int i, i_, ln = len(x) - 1;
+
+
+	for (i = 0, i_ = ln; i < ln / 2; i++, i_--)
+	{
+		int tmp = x[i];
+
+		x[i] = x[i_];
+		x[i_] = tmp;
+	}
+	return (x);
+}
+/**
+ * infinite_add - adds two strings of numbers return
+ * convert them partially to number then sring
+ * @n1: type char* str to add
+ * @n2: type char* str to add
+ * @r:  type cgar* str to add to
+ * @size_r: maximum buffer size
+ * Return: sum of umbers as char* r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	/*declare counter varables*/
-	int i, i1, i2, i3, i4, i5;
+	/* countera variables */
+	int i1, i2, r_ = 0;
+	/*lengthes*/
+	int ln1 = len(n1), ln2 = len(n2);
+	/*arithmetic variables sones ones sum ctens to carry tens*/
+	int sones = 0, ctens = 0;
 
-	for (i = 0; n1[i]; i++)
-	;
-
-	for (i1 = 0; n2[i1]; i1++)
-	;
-
-	/*checks if either i or j is greater than size_r.*/
-	if (i > size_r || i1 > size_r)
+	/*not Exceed size_r*/
+	if (size_r < ln1 || size_r < ln2)
 	{
 		return (0);
 	}
-	i4 = 0;
-	/*loop will stop when it reaches the limit of the buffer size*/
-	for (i -= 1, i1 -= 1, i2 = 0; i2 < size_r - 1; i--, i1--, i2++)
+	for (i1 = ln1 - 1, i2 = ln2 - 1; r_ < size_r - 1; i1--, i2--)
 	{
-		i5 = i4;
-		if (i >= 0)
+		/*add carred tens to sumation of ones*/
+		sones = ctens;
+		if (i1 >= 0)
 		{
-			i5 += n1[i] - '0';
+			sones += n1[i1] - '0';
 		}
-			if (i1 >= 0)
-			{
-				i5 += n2[i1] - '0';
-			}
-		/* If this is true, it breaks out of the loop.*/
-		if (i < 0 && i1 < 0 && i5 == 0)
+		if (i2 >= 0)
+		{
+		sones += n2[i2] - '0';
+		}
+		if (i1 < 0 && i2 < 0 && ctens == 0)
 		{
 			break;
 		}
-		i4 = i5 / 10;
-		r[i2] = i5 % 10 + '0';
+		r[r_] = (sones % 10) + '0';
+		ctens = sones / 10;
+		r_++;
 	}
-	r[i2] = '\0';
-	if (i >= 0 || i1 >= 0 || i4)
-		return (0);
-	for (i2 -= 1, i3 = 0; i3 < i2; i2--, i3++)
+	if (i1 >= 0 || i2 >= 0 || ctens != 0)
 	{
-		i4 = r[i2];
-		r[i2] = r[i3];
-		r[i3] = i4;
+		return (0);
 	}
+	/*adding \0 then  off finaly  get r in to corect order */
+	r[r_] = '\0';
+	stringRevers(r);
 	return (r);
-
 }
+
