b21ae3f043f9c94d61e9214446d449f15cce6b82
MoedCode <mohamed71291@gmail.com>
2023-07-03 14:18:04 -0700

    ال جي احلي او احا علي حسب الظروف (automated command)

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ *  _memcpy - copy buffers
+ * @dest: the address of memory to source
+ * @dest: the address of memory to distnation
+ * @n: the size of the memory to print
+ *
+ * Return: dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
