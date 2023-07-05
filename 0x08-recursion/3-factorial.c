523fdf2def6570d8105aa4482c8b0dd2f5e15fd1
MoedCode <mohamed71291@gmail.com>
2023-07-05 11:08:11 -0700

    (automated command)

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+/**
+ * factorial - return factorial of a given number using recursion
+ *
+ * @n:number to be calculated
+ *
+ * Return: factorial number
+ */
+int factorial(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	if (n > 2)
+		n *= factorial((n - 1));
+		return (n);
+	return (n);
+}
