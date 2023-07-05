78d4e3fe802e57cc21ee910ab2ea13533a6c423f
MoedCode <mohamed71291@gmail.com>
2023-07-05 07:17:56 -0700

    (automated command)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,12 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		write(1, s, 1);
+		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
 	else
 	{
-		write(1, s, 1);
-		return;
+		_putchar('\n');
 	}
-	}
+}
