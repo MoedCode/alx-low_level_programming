0ba1bdfae666ef1b4c0fe142bdc84a46182caf0e
MoedCode <mohamed71291@gmail.com>
2023-07-05 09:53:21 -0700

    (automated command)

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints strings in reverse using recurive
+ * and putchar
+ *
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
+
