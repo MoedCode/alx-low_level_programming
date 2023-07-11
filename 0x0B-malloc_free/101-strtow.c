08e1a15b42761860d85ac0f4b3ef6505f4f995cf
MoedCode <mohamed71291@gmail.com>
2023-07-11 16:25:59 -0700

    (automated command)

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * wordCount _ count words in a string  return worf number
+ * wordCount - count words in a string  return worf number
  * @str: type char* dtring to be count
- * Return int
+ * Return: int
 */
 
 int wordCount(char *str)
@@ -15,8 +15,8 @@ int wordCount(char *str)
 
 	for (; str[chars] != '\0'; chars++)
 	{
-		tmp1 = chars +1;
-		if (str[chars] != ' '&&  str[tmp1 ] == ' ')
+		tmp1 = chars + 1;
+		if (str[chars] != ' ' &&  str[tmp1] == ' ')
 		{
 			words++;
 		}
@@ -36,55 +36,30 @@ char **strtow(char *str)
 	int i, j, k, ln, first, end;
 
 	if (str == NULL || str[0] == '\0')
-	{
 		return (NULL);
-	}
-
 	wNum = wordCount(str);
 	if (wNum == 0)
-	{
 		return (NULL);
-	}
-
-
-	words = malloc((wNum + 1) * sizeof(char *));
+	words = malloc((wNum) * sizeof(char *));
 	if (words == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0, j = 0; str[i] != '\0';)
 	{
 		while (str[i] == ' ')
-		{
 			i++;
-		}
-
 		first = i;
-
 		while (str[i] != ' ' && str[i] != '\0')
-		{
 			i++;
-		}
-
 		end = i;
-
 		ln = end - first;
 		words[j] = malloc((ln + 1) * sizeof(char));
 		if (words[j] == NULL)
-		{
 			return (NULL);
-		}
-
 		for (k = 0; k < ln; k++)
-		{
 			words[j][k] = str[first + k];
-		}
 		words[j][k] = '\0';
-
 		j++;
 	}
-
 	words[j] = NULL;
-
 	return (words);
 }
