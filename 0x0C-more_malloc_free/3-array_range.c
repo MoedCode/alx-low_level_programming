78e36f2a5a2a4e118e1a4550ac905de046cf9760
MoedCode <mohamed71291@gmail.com>
2023-07-13 18:32:31 -0700

    malloc 0x0C (automated command)

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * array_range -  allocat memory to\creates an array of integers
+ * @min: int minimum  values
+ * @max: int maximum values
+ * Return: int (pointer)to allocated array,(NULL) on falier
+ */
+int *array_range(int min, int max)
+{
+	int *intArr, i;
+
+	if (max < min)
+		return (NULL);
+
+	intArr = malloc(sizeof(int *) * ((max - min) + 1));
+	if (intArr == NULL)
+		return (intArr);
+	for (i = 0; min <= max; min++, i++)
+		intArr[i] = min;
+
+	return (intArr);
+}
