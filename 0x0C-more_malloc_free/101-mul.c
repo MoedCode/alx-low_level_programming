d8df825d936c8e27a995b80c67d6985560d46124
MoedCode <mohamed71291@gmail.com>
2023-07-15 20:56:43 -0700

    (automated command)

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,9 +7,10 @@
  * @s: char* string to calculate its length
  * Return: string length
  */
-int  _strlen(char *s )
+int  _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -24,45 +25,43 @@ int  _strlen(char *s )
  *
  *Return: (char* buff) reslt of multiplication ,(NULL) if falid to allocate
  */
-char* lmitlessMul(char* buff0, char* buff1)
+char *lmitlessMul(char *buff0, char *buff1)
 {
-	int i0, i1, ln0, ln1, ln2, carry = 0, ones;
-	char* buff;
+	int i0, i1, ln0, ln1, ln2, carry = 0, ones, num1 = 0;
+	char *buff;
 
 	ln0 = _strlen(buff0);
 	ln1 = _strlen(buff1);
 	ln2 = ln0 + ln1 + 1;
-	buff = (char*) calloc(ln2, sizeof(char));
+	buff =  calloc(ln2, sizeof(char));
 	if (buff == NULL)
-		return buff;
+		return (buff);
 	for (i0 = 0; i0 < ln2; i0++)
 		buff[i0] = '0';
-	for (i0 = ln0 - 1; i0 >= 0; i0--) {
-		int num1 = 0;
-
+	for (i0 = ln0 - 1; i0 >= 0; i0--)
+	{
+		num1 = 0;
 		num1 = buff0[i0] - '0';
 		carry = 0;
-
-		for (i1 = ln1 - 1; i1 >= 0; i1--) {
+		for (i1 = ln1 - 1; i1 >= 0; i1--)
+		{
 			int num2;
 
 			num2 = buff1[i1] - '0';
-			 ones = 0;
+			ones = 0;
 			ones =  num1 * num2 + (buff[i0 + i1 + 1] - '0') + carry;
 			buff[i0 + i1 + 1] = (ones % 10) + '0';
 			carry = ones / 10;
 		}
 		buff[i0 + i1 + 1] = carry + '0';
 	}
-
 	buff[ln2 - 1] = '\0';
+	if (*buff == '0')
+		return (buff + 1);
 
-	if (*buff == '0') {
-		return buff + 1;
-	}
-	else {
+	else
 		return (buff);
-	}
+
 }
 /**
  * main - Entry point
@@ -84,8 +83,8 @@ int main(int argc, char *argv[])
 
 
 
-	result =lmitlessMul(buff0, buff1);
-	printf("%s",result);
+	result = lmitlessMul(buff0, buff1);
+	printf("%s", result);
 	free(result);
 	return (0);
 
