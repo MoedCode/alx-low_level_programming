3ec9f766b117fa59239a561118b85e50a176b30b
MoedCode <mohamed71291@gmail.com>
2023-07-19 19:54:26 -0700

    FUCTION POINTERS Tasks (automated command)

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ *@argc: argv[] length
+ *@argv: char* array of nputs 
+ *
+ * Return: (0) on success
+ */
+int main(int argc, char *argv[])
+{
+	int i , n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	 n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	unsigned char *fp = (char *)main;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", fp[i]);
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
+
