f995d5a74ea72579f8058fa881fc5c4313fa1725
MoedCode <mohamed71291@gmail.com>
2023-07-19 17:41:29 -0700

    (automated command)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * int_index -  searches for an integer
  *
@@ -13,6 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (cmp == NULL || array == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
