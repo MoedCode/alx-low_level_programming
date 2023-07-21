6b32f149a5e9a23fa4f14a3d985d28fcce2e4c4e
MoedCode <mohamed71291@gmail.com>
2023-07-20 19:20:33 -0700

    (automated command)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,10 +22,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (s)
 			printf("%s", s);
 		else
-		 printf("(nil)");
+			printf("(nil)");
 		if (i < (n - 1) && separator)
 		{
 			putchar(separator[0]);
+			putchar(32);
 		}
 	}
 	va_end(args);
