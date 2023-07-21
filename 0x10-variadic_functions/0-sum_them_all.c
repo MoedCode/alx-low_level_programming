93c3542cf08566144ad8e8f03186a938b70aadcc
MoedCode <mohamed71291@gmail.com>
2023-07-20 17:52:29 -0700

    variadic_functions (automated command)

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ *
+ * @n: parameter number
+ * @...: parameters
+ * Return: (int) sum of the all arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	size_t i, result = 0;
+	int tmp;
+	va_list args;
+
+	if (n == 0)
+		return (n);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = 0;
+		tmp = va_arg(args, int);
+		result  += tmp;
+	}
+	va_end(args);
+	return (result);
+
+
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* Packages */
+#include <stdarg.h>
+#include <stddef.h>
+#include<stdio.h>
+#include<string.h>
+
+/* Prototypes */
+int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif 
