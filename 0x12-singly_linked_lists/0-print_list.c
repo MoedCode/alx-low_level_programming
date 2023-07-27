c6c0026474593364d7b78d2aff603649bbd340b0
MoedCode <mohamed71291@gmail.com>
2023-07-27 16:57:43 -0700

    (automated command)

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stddef.h>
 /**
  *print_list  - prints all the elements of a list_t list.
  * @h: type list_t , the list to print
@@ -6,16 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	int i = 0;
-	for (; h ;)
+	size_t i = 0;
+
+	for (; h != NULL;)
 	{
-		if (*h->str)
+		if (h->str != NULL)
 			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 	i++;
 	h = h->next;
+
 	}
 	return (i);
-
 }
