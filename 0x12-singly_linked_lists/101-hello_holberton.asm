79aec7237b481b5621415cbb3617aa9465215db2
MoedCode <mohamed71291@gmail.com>
2023-07-27 19:56:12 -0700

    (automated command)

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,22 @@
-section .data
-	msg db "Hello, Holberton", 0
-	fmt db "%s", 10, 0
+extern printf ; declare printf as an external function
 
-section .text
-	extern printf
-	global main
+section .text ; start of the text section
+	global main ; declare main as a global symbol
 
-main:
-	; >> arg for printf onto the stack
-	push dword msg
-	push dword fmt
+main: ; start of the main function
+	push rbp ; save the base pointer on the stack
 
-	; call printf
-	call printf
+	mov rax,0 ; set rax to 0, indicating no floating-point arguments
+	mov rdi,fmt ; set rdi to the address of the format string
+	mov rsi,msg ; set rsi to the address of the message string
+	call printf ; call the printf function
 
-	; clean up stack
-	add esp, 8
+	pop rbp ; restore the base pointer from the stack
 
-	; exit  status code 0
-	xor eax, eax
-	ret
+	xor rax,rax ; set rax to 0, indicating success
+	ret ; return from the main function
+
+section .data ; start of the data section
+	msg: db "Hello, Holberton", 10, 0 ; define a null-terminated string with a newline
+	fmt: db "%s", 0 ; define a null-terminated format string for printf
+
