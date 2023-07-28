9d57d566550384b3223b15fc81750342f8e2276d
MoedCode <mohamed71291@gmail.com>
2023-07-27 17:47:24 -0700

    (automated command)

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len -  return number of nodes in a linked list
+ * @h: pointer to first node
+ * Return:(i) size_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	for (; h;)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+iii}
