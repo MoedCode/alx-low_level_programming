06fa0707680a1ab8dd0588f3cbb31fd742fda3f1
MoedCode <mohamed71291@gmail.com>
2023-08-01 08:08:14 -0700

    (automated command)

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,36 +1,35 @@
 #include "lists.h"
 /**
- * add_nodeint - node at the end of a listint_t list.
+ * add_nodeint_end - node at the end of a listint_t list.
  *
  * @head:ponter pointer of list
  * @n: integer value to be assige for node
- * @return (listint_t*) pointer to added npde
+ * Return: (listint_t*) pointer to added npde
  */
- listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
 	listint_t *node, *tmp;
 
-	if (!*head)
-		*head = node;
-
 	node = malloc(sizeof(listint_t));
-	if(!node)
+	if (!node)
 		return (node);
+
 
 	node->n = n;
 	node->next = NULL;
 
 	if (!*head)
 		*head = node;
+
 	else
 		{
-		 while (tmp->next)
+		tmp = *head;
+		while (tmp->next)
 			tmp = tmp->next;
 		tmp->next = node;
 		}
-*head = node;
-return (node);
 
+return (node);
 
 }
