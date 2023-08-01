7e45499d8b419cf3d5eb7b00497f2ef3c40ccc47
MoedCode <mohamed71291@gmail.com>
2023-08-01 09:18:11 -0700

    (automated command)

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,12 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp, *head_ = *head;
+	listint_t *tmp;
 
-	while (head_)
+	while (*head)
 	{
-		tmp = head_->next;
-		free(head_);
-		head_ = tmp;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
 }
