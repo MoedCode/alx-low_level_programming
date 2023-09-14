32524089058490c2bd48ee98100a7f07e2acfe4f
MoedCode <mohamed71291@gmail.com>
2023-09-14 23:55:43 +0300

    T3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * Add_Head -  adds a new node
+ * with null previous and next filelds
+ * @H: a null pointer to an empty list
+ * @n: element to be added in the linked list as the new head
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *Add_Head(dlistint_t **H, int n)
+{
+	dlistint_t *N = malloc(sizeof(dlistint_t));
+
+	if (!N)
+		return (NULL);
+	N->n = n;
+	N->next = NULL;
+	N->prev = NULL;
+	*H = N;
+	return (N);
+}
+/**
+ * add_dnodeint_end -  adds a new node at the  end  of
+ *a dlistint_t list.
+ *
+ * @head: pointer to pointer to head
+ * @n: new node integer value
+ * Return: dlistint_t* new added node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *NODE, *HEAD  = *head;
+
+		if (!head)
+			return (NULL);
+		if (!*head)
+			return (Add_Head(head, n));
+	NODE = malloc(sizeof(dlistint_t) * 1);
+	if (!NODE)
+		return (NULL);
+	while (HEAD->next)
+		HEAD = HEAD->next;
+	/* NRW NODE*/
+	NODE->n = n;
+	NODE->prev = HEAD;
+	NODE->next = NULL;
+	/* (*head)->prev = NODE; */
+	HEAD->next = NODE;
+
+
+
+	return (NODE);
+}
+
