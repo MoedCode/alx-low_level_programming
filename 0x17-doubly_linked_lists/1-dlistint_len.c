9948d35e99585a74720e6b4315fbe4ba050269b9
MoedCode <mohamed71291@gmail.com>
2023-09-14 20:02:56 +0300

    T1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *print_dlistint - return  singaly list length
+ * @h: pointer to first node
+ * Return: size_t i whics number of  nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+	const dlistint_t *H = h;
+
+	for (i = 0; H; i++)
+	{
+		H = H->next;
+	}
+	return (i);
+}
+
