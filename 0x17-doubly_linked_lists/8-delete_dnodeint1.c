ab0d24ddc859e862ad12dc3fddb64b404ceee821
MoedCode <mohamed71291@gmail.com>
2023-09-15 19:13:13 +0300

    T8

--- 0x17-doubly_linked_lists/8-delete_dnodeint1.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint1.c
@@ -2,45 +2,53 @@
 #include <stddef.h>
 
 /**
- * delete_dnodeint_at_index - delete a node at a given position
+ * delete_dnodeint_at_index - delet a  node at a given position
  *
- * @head: double pointer to head node
+ * @head:double pointer to Hrad node
  * @index: the given position
- * Return: (-1) if can't delete node, else (1)
+ * Return:(-1) if cant delet node , else (1)
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
+
+	size_t i = 0;
 	dlistint_t *H = *head, *tmpH;
 
 	if (*head == NULL)
-		return index == 0 ? 1 : -1;
+		return 1;
 
 	if (index == 0)
 	{
+		tmpH = H;
+		H->prev = NULL;
 		*head = H->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(H);
+		free(tmpH);
 		return 1;
 	}
 
-	while (H != NULL)
+	for (; H; H = H->next, i++)
 	{
-		if (i == index)
+
+		if ((H->next == NULL) && (i == index))
 		{
+
 			tmpH = H;
-			H->prev->next = H->next;
-			if (H->next != NULL)
-				H->next->prev = H->prev;
+			H->prev->next = NULL;
 			free(tmpH);
+
 			return 1;
 		}
-		if (H->next == NULL && i < index)
-			return -1;
 
-		H = H->next;
-		i++;
+		if (i == index)
+		{
+
+			tmpH = H;
+
+			H->prev->next = H->next;
+			H->next->prev = H->prev;
+			free(tmpH);
+			return (1);
+		}
 	}
 	return -1;
 }
