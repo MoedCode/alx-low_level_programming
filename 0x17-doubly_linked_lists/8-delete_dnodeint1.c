003c0bdfb74d059f2c782dbb32151bac9b5ee567
MoedCode <mohamed71291@gmail.com>
2023-09-15 18:57:54 +0300

    T8

--- 0x17-doubly_linked_lists/8-delete_dnodeint1.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint1.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 #include <stddef.h>
-/**
- * Add_Head -  adds a new node
- * with null previous and next filelds
- * @H: a null pointer to an empty list
- * @n: element to be added in the linked list as the new head
- * Return: the address of the new element, or NULL if it failed
- */
-dlistint_t *Add_Head(dlistint_t **H, int n)
-{
-	dlistint_t *N = malloc(sizeof(dlistint_t));
 
-	if (!N)
-		return (NULL);
-	N->n = n;
-	N->next = NULL;
-	N->prev = NULL;
-	*H = N;
-	return (N);
-}
 /**
  * delete_dnodeint_at_index - delet a  node at a given position
  *
@@ -37,10 +19,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		tmpH = H;
-		H->prev = NULL;
 		*head = H->next;
-		free(tmpH);
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(H);
 		return 1;
 	}
 
