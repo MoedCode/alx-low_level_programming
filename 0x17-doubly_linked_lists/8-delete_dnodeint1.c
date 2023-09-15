5fcf60fc15c180637f6f71c27e9c544e35d03807
MoedCode <mohamed71291@gmail.com>
2023-09-15 05:22:45 +0300

    T8

--- 0x17-doubly_linked_lists/8-delete_dnodeint1.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint1.c
@@ -9,45 +9,39 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	size_t i;
+	dlistint_t *tmpH, *H = *head;
 
-	size_t i = 0;
-	dlistint_t *H = *head, *tmpH;
-
-	if (*head == NULL)
-		return 1;
-
-	if (index == 0)
-	{
-		tmpH = H;
-		H->prev = NULL;
-		*head = H->next;
-		free(tmpH);
-		return 1;
-	}
-
-	for (; H; H = H->next, i++)
-	{
-
-		if ((H->next == NULL) && (i == index))
+		if (*head == NULL)
+			return (1);
+		if (index == 0)
 		{
-
 			tmpH = H;
-			H->prev->next = NULL;
+			H->prev = NULL;
+			*head = H->next;
 			free(tmpH);
+			return (1);
 
-			return 1;
 		}
+	for (i = 0; H; i++, H = H->next)
+	{
 
-		if (i == index)
+		if (index == i)
 		{
-
 			tmpH = H;
-
 			H->prev->next = H->next;
 			H->next->prev = H->prev;
 			free(tmpH);
 			return (1);
 		}
+		if (!H->next && i == index)
+		{
+			tmpH = H;
+			H->prev->next = NULL;
+			free(tmpH);
+			return (1);
+		}
 	}
-	return -1;
+	return (-1);
+
 }
