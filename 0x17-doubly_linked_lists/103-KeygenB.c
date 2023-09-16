c5fe9ab4fc945361d5f3b6d29329f7ce1e956c35
MoedCode <mohamed71291@gmail.com>
2023-09-16 05:19:36 +0300

    T11

--- 0x17-doubly_linked_lists/103-KeygenB.c
+++ 0x17-doubly_linked_lists/103-KeygenB.c
@@ -3,21 +3,18 @@
 #include <stdlib.h>
 
 /**
- * main - program entry point
- * program that grnerate key from user name  to crack5 file
- *"For the good of humanity 😂"
- * @argc: integer value of arguments
- * @argv:double array of  arguments passed to main
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
  *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
 	unsigned int i, b;
-	size_t len, ASCIISUM;
-	char p[7] = "      ",
-	*CAHRS = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char p[7] = "      ";
 
 	if (argc != 2)
 	{
@@ -25,24 +22,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	len = strlen(argv[1]);
-	p[0] = CAHRS[(len ^ 59) & 63];
-	for (i = 0, ASCIISUM = 0; i < len; i++)
-		ASCIISUM += argv[1][i];
-	p[1] = CAHRS[(ASCIISUM ^ 79) & 63];
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
 	for (i = 0, b = 1; i < len; i++)
 		b *= argv[1][i];
-	p[2] = CAHRS[(b ^ 85) & 63];
+	p[2] = l[(b ^ 85) & 63];
 	for (b = argv[1][0], i = 0; i < len; i++)
 		if ((char)b <= argv[1][i])
 			b = argv[1][i];
 	srand(b ^ 14);
-	p[3] = CAHRS[rand() & 63];
+	p[3] = l[rand() & 63];
 	for (b = 0, i = 0; i < len; i++)
 		b += argv[1][i] * argv[1][i];
-	p[4] = CAHRS[(b ^ 239) & 63];
+	p[4] = l[(b ^ 239) & 63];
 	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
 		b = rand();
-	p[5] = CAHRS[(b ^ 229) & 63];
+	p[5] = l[(b ^ 229) & 63];
 	printf("%s\n", p);
 	return (0);
 }
