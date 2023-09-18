cdd69fa315fc15ab0d165794f8ee791211828374
MoedCode <mohamed71291@gmail.com>
2023-09-19 02:29:03 +0300

    T2 Advanc (automated command)

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - a function that subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - a function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - a function that divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the quotient of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - a function that finds the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the remainder of the division of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
