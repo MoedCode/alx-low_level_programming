a8f21bda4eda16f8f47043d1c76c1fe49f0ec4cc
MoedCode <mohamed71291@gmail.com>
2023-09-19 05:01:04 +0300

    T3 Advanc in prog (automated command)

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget https://github.com/MoedCode/alx-low_level_programming/tree/master/0x18-dynamic_libraries
+wget -p /tmp https://github.com/MoedCode/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libgiga.so
+exports LD_PRELOAD=/tmp/libgiga.so
