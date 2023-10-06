9fd8f02bb7ae59ca6da43e56c435ddf58410d46e
MoedCode <mohamed71291@gmail.com>
2023-10-06 23:48:15 +0300

    👉 👈 Every thing grat no really T3 hash table (automated command)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,8 +4,8 @@
 /**
  * hash_table_set - Adds an element to the hash table's array.
  * @ht: Pointer to the hash table.
- * @key: Key of the hash table array, which is an array of nodes where each node
- * is a key-value pair.
+ * @key: Key of the hash table array, which is an
+ * array of nodes where each node is a key-value pair.
  * @value: Value associated with the key.
  *
  * Return: (1) if succeeded, (0) for any failure.
@@ -18,9 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !ht->array || !ht->size || !key || !value)
 		return (0);
-
 	hashIdx = key_index((unsigned char *)key, ht->size);
-
 	/* Check if the key already exists */
 	for (cur_node = ht->array[hashIdx]; cur_node; cur_node = cur_node->next)
 	{
@@ -31,15 +29,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-
 	/* If the key does not exist previously */
 	new_node = malloc(sizeof(*new_node));
 	if (!new_node)
 		return (0);
-
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-
 	if (!new_node->key || !new_node->value)
 	{
 		if (new_node->key)
@@ -49,7 +44,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[hashIdx];
 	ht->array[hashIdx] = new_node;
 
--- 0x1A-hash_tables/add2hash.c
+++ 0x1A-hash_tables/add2hash.c
@@ -1,42 +1,62 @@
 #include "hash_tables.h"
-#include <string.h>
+#include <stddef.h>
+
+/**
+ * hash_table_set - Adds an element to the hash table's array.
+ * @ht: Pointer to the hash table.
+ * @key: Key of the hash table array, which is an array of nodes where each node
+ * is a key-value pair.
+ * @value: Value associated with the key.
+ *
+ * Return: (1) if succeeded, (0) for any failure.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
 
-	hash_node_t *cur_node, *new_node;
-	unsigned long int hashIdx;
-	if (!ht || !ht->array || !ht->size || !key  || !value)
+	if (!ht || !key || !key[0] || !value)
 		return (0);
-	hashIdx = key_index((unsigned char *)key, ht->size);
-	;
-	/* incase of key  is allready wxsist*/
-	for (cur_node = ht->array[hashIdx]; cur_node->next; cur_node = cur_node->next)
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	current_node = ht->array[index];
+
+	/* Check if key already exists, update the value if so */
+	while (current_node)
 	{
-		if(!strcmp(key, cur_node->key))
+		if (strcmp(current_node->key, key) == 0)
 		{
-			free(cur_node->value);
-			cur_node->value = strdup(value);
+			free(current_node->value);  /* Free the existing value */
+			current_node->value = strdup(value);  /* Duplicate the new value */
+			if (!current_node->value)
+				return (0);
 			return (1);
 		}
-	}
-	/*if key does not exsist previously*/
-	new_node =(hash_node_t*)malloc(sizeof(*new_node));
-			if(!new_node)
-				return (0);
-	new_node->key = strdup(key);
-	new_node->value=strdup(value);
-	if(!new_node->key || !new_node->value)
-	{
-		if(new_node->key)
-			free(new_node->key);
-		if(new_node->value)
-			free(new_node->value);
-	return (0);
+		current_node = current_node->next;
 	}
 
-	new_node->next = ht->array[hashIdx];
-	ht->array[hashIdx] = new_node;
+	/* Create a new node and insert it at the beginning of the list */
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (!new_node->key || !new_node->value)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
+
 }
