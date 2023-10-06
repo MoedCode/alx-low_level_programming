27dac7315b7079cda2ef6786cb4a96811836c11a
MoedCode <mohamed71291@gmail.com>
2023-10-06 18:26:51 +0300

    no way ▶ (automated command)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,20 +12,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *TABLE;
 
 	/*if (size < 1024 || !(TABLE = malloc((sizeof(*TABLE)))));*/
-	if(!size)
+	if (!size)
 		return (NULL);
-	TABLE = malloc(sizeof (hash_table_t));
+	TABLE = malloc(sizeof(hash_table_t));
 	if (!TABLE)
 		return (NULL);
 
 	TABLE->size = size;
-	TABLE->array = malloc(sizeof(hash_node_t ) * size);
-	if(!TABLE->array)
+	TABLE->array = malloc(sizeof(hash_node_t) * size);
+	if (!TABLE->array)
 	{
 		free(TABLE);
 		return (NULL);
 	}
-	for (i=0 ; i< TABLE -> size ; ++i)
+	for (i = 0; i < TABLE->size; ++i)
 		TABLE->array = NULL;
 
 	return (TABLE);
