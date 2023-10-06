5c5b136152e1628a9494c34e68ba1a1612ed47e1
MoedCode <mohamed71291@gmail.com>
2023-10-06 22:41:00 +0300

    👉 👈 Every thing grat no really T3 hash table (automated command)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *current_node;
 
-	if (!ht || !key || !key[0] || !value)
+	if (!ht || !ht->array || !ht->size ||!key  || !value)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
