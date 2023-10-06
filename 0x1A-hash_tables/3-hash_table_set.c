01e05c4948c93fa65a1366e53d70c5a1560a52c9
MoedCode <mohamed71291@gmail.com>
2023-10-06 21:23:57 +0300

    👉 👈 Every thing grat no really T3 hash table (automated command)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,48 +1,15 @@
 #include "hash_tables.h"
+#include <stddef.h>
 
-/**
- * hash_table_set - adds an element to the hash table
- * @ht: is the hash table you want to add or update the key/value to
- * @key: is the key. key can not be an empty string
- * @value: is the value associated with the key.
- * Return: Returns: 1 if it succeeded, 0 otherwise
- */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *temp;
-	hash_node_t *hsnode;
+	unsigned long int hashIdx;
 
-	if (ht == NULL)
+	if (!ht || !ht->array || !ht->size || !key || !key[0])
 		return (0);
-	if (key == NULL || key[0] == '\0')
-		return (0);
-	if (ht->size == 0 || ht->array == NULL)
-		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	temp = ht->array[index];
-	while (temp != NULL)
-	{
-		if (strcmp(temp->key, key) == 0)
-		{
-			free(temp->value);
-			temp->value = strdup(value);
-			return (1);
-		}
+	hashIdx = key_index((unsigned char *)key, ht->size);
 
-		temp = temp->next;
-	}
-	hsnode = malloc(sizeof(hash_node_t));
-	if (hsnode == NULL)
-		return (0);
-	hsnode->key = strdup(key);
-	hsnode->value = strdup(value);
-	if (ht->array[index] != NULL)
-	{
-		hsnode->next = ht->array[index];
-	}
-	else
-		hsnode->next = NULL;
-	ht->array[index] = hsnode;
-	return (1);
+
+
+
 }
