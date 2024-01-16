adcdeefdf46d14c22e85dda50cd8454dc6d2dc95
MoedCode <mohamed71291@gmail.com>
2024-01-16 14:29:59 +0200

    0. Linear search (automated command)

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,31 +1,27 @@
+#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
+ * linear_search -searches for a value in given int array
+ *using the Linear search algorithm
  *
- * @array: input array
+ * @array: pointer to first index in integers array
  * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * @value: Value to search in the concern array
+ * Return: (int) index if element that mach the concern value
+ * or (-1) if that value not fond or null array pointer
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
-	if (array == NULL || size == 0)
-	{
+	if (!array || !size)
 		return (-1);
-	}
-
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%i] = [%d]\n", (int)i, array[i]);
 		if (array[i] == value)
-		{
 			return (i);
-		}
 	}
-	return (-1);
+	return  (-1);
 }
