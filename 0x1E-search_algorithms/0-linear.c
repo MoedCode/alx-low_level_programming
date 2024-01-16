76221293ffedcaf83687c6bfb2ed05ba4a8282eb
MoedCode <mohamed71291@gmail.com>
2024-01-16 13:54:30 +0200

    0x1E-search_algorithms 0. Linear search (automated command)

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search -searches for a value in given int array
+ *using the Linear search algorithm
+ *
+ * @array: pointer to first index in integers array
+ * @size: size of the array
+ * @value: Value to search in the concern array
+ * Return: (int) index if element that mach the concern value
+ * or (-1) if that value not fond or null array pointer
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%i] = [%d]\n", (int)i, array[i]);
+
+		if (array[i] == value)
+		{
+		/*	printf("Found %d at index: %d\n", array[i], (int)i); */
+			return (i);
+		}
+
+	}
+	return  (-1);
+
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("GCC Working");
+	return (0);
+}
