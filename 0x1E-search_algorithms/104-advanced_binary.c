26ec6087b787c66b11631fb1cac59df27c5830a7
MoedCode <mohamed71291@gmail.com>
2024-01-18 03:29:16 +0200

    11. Advanced binary search (automated command)

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,14 +1,62 @@
-#include <stdio.h>
+#include "search_algos.h"
+
 /**
- *   -
+ * custom_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * @array: pointer to first index in integers array
- * @size: size of the array
- * @value: Value to search in the concern array
- * Return: (int) index if element that mach the concern value
- * or (-1) if that value not fond or null array pointer
+ * @arr: input array
+ * @sz: size of the array
+ * @val: value to search in
+ * Return: index of the number
  */
+int custom_search(int *arr, size_t sz, int val)
 {
-	if (!array || !size)
+	size_t mid = sz / 2, i;
+
+	if (!arr || !sz)
 		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < sz; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", arr[i]);
+
+	printf("\n");
+
+	if (mid && sz % 2 == 0)
+		mid--;
+
+	if (val == arr[mid])
+	{
+		if (mid > 0)
+			return (custom_search(arr, mid + 1, val));
+		return ((int)mid);
+	}
+
+	if (val < arr[mid])
+		return (custom_search(arr, mid + 1, val));
+
+	mid++;
+	return (custom_search(arr + mid, sz - mid, val) + mid);
+}
+
+/**
+ * advanced_binary - calls to custom_search to return
+ * the index of the number
+ *
+ * @arr: input array
+ * @sz: size of the array
+ * @val: value to search in
+ * Return: index of the number
+ */
+int advanced_binary(int *arr, size_t sz, int val)
+{
+	int idx;
+
+	idx = custom_search(arr, sz, val);
+
+	if (idx >= 0 && arr[idx] != val)
+		return (-1);
+
+	return (idx);
 }
